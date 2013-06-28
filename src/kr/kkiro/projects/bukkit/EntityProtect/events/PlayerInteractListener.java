8e657b6ba2119b117802432d37a6246b5d947893
yoo2001818 <yooduknam@naver.com>
2013-06-28 17:17:11 +0900

    Created Breeding check method

--- src/kr/kkiro/projects/bukkit/EntityProtect/events/PlayerInteractListener.java
+++ src/kr/kkiro/projects/bukkit/EntityProtect/events/PlayerInteractListener.java
@@ -1,10 +1,12 @@
 package kr.kkiro.projects.bukkit.EntityProtect.events;
 
+import kr.kkiro.projects.bukkit.EntityProtect.utils.BreedChecker;
 import kr.kkiro.projects.bukkit.EntityProtect.utils.ChatUtils;
 import kr.kkiro.projects.bukkit.EntityProtect.utils.EntityUtils;
 import kr.kkiro.projects.bukkit.EntityProtect.utils.database.DatabaseUtils;
 import kr.kkiro.projects.bukkit.EntityProtect.utils.database.EntitySet;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -18,6 +20,7 @@ public class PlayerInteractListener implements Listener {
 	public void onPlayerInteractEntity(PlayerInteractEntityEvent e) {
 		Player player = e.getPlayer();
 		Entity entity = e.getRightClicked();
+		Material material = player.getItemInHand().getType();
 		if (entity == null)
 			return;
 		if (!EntityUtils.isEnabled(entity.getType()))
@@ -28,6 +31,9 @@ public class PlayerInteractListener implements Listener {
 			ChatUtils.sendLang(player, "owner-info", 
 					"#mobs."+entity.getType().getName(),
 					(owner == player.getName()) ? "#you" : owner);
+		}
+		if (BreedChecker.check(entity.getType(), material)) {
+			PermissionUtils.
 		}
 	}
 
--- src/kr/kkiro/projects/bukkit/EntityProtect/utils/PermissionUtils.java
+++ src/kr/kkiro/projects/bukkit/EntityProtect/utils/PermissionUtils.java
@@ -5,13 +5,13 @@ import kr.kkiro.projects.bukkit.EntityProtect.utils.config.Config;
 import org.bukkit.entity.Player;
 
 public class PermissionUtils {
-	public boolean canBypass(String activity, Player player, Boolean hasOwner) {
+	public static boolean canBypass(String activity, Player player, Boolean hasOwner) {
 		if(player.hasPermission("entityprotect.bypass-protect."+activity)) return true;
 		if(hasOwner && Config.getString("protect-entities."+activity).equals("false")) return true;
 		if(Config.getString("protect-entities."+activity).equals("nonowner")) return false;
 		return false;
 	}
-	public boolean canBypass(String activity, Boolean hasOwner) {
+	public static boolean canBypass(String activity, Boolean hasOwner) {
 		return !Config.getBoolean("protect-entities."+activity);
 	}
 }
